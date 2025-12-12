5f426ce3386113a0796dbe0735ec9b0c4b0f3253
le0hr <lev.hrabovij@gmail.com>
2025-12-12 23:34:14 +0200

    remaining time calculation added

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 import messageHandler as mh
 import asyncio
-# read setup file
-print(10)
+from datetime import datetime as dt
+from datetime import timedelta as timed
+
+
+# Сheck if curently in period
+def is_shuted(period: str) -> bool:
+    now = dt.now().time()
+    for i in period:
+        start, end = i
+
+        if start == "24:00":
+            end_time = dt.time(23, 59, 59)
+        else:
+            end_time = dt.strptime(end, "%H:%M").time()
+            
+        start_time = dt.strptime(start, "%H:%M").time()
+        if start_time <= now <= end_time:
+            return True
+    return False
+
+# Calculete the remaining time
+def time_remaning(period: str, is_shuted: bool) -> int:
+    # Supportive variable, which helps to find a time difference
+    dummy_date = dt.date(dt(1,1,1))
+    now = dt.combine(dummy_date, dt.now().time())
+    for time in period:
+        diff = dt.combine(dummy_date, dt.strptime(time[is_shuted], "%H:%M").time())-now
+        diff = diff.total_seconds()       
+        if diff >0:
+            return diff
+
+
 async def main():
-    with open('./config.txt') as file:
-        api_id = file.readline().split('=')
-        api_hash = file.readline().split('=')
+    # Read setup file
+    with open('./config.cfg') as file:
+        api_id = int(file.readline().removeprefix('api_id='))
+        api_hash = file.readline().removeprefix('api_hash=').replace('\r\n', '').replace('\n', '')
+        source = int(file.readline().removeprefix('source='))
+        shutdown_query = file.readline().removeprefix('shutdown_query=').replace('\r\n', '').replace('\n', '')
+        message_pattern = file.readline().removeprefix('message_pattern=').replace('\r\n', '').replace('\n', '')
+    async for shutdown_period in mh.get_shutdown_periods(api_id,api_hash,source,shutdown_query,message_pattern):
+        print(time_remaning(shutdown_period, is_shuted(shutdown_period)))
 
-        api_id = int(api_id[1])
-        api_hash=api_hash[1]
-        
-    async for shoutdown_time in mh.get_shoutdown_time(api_id, api_hash):
-        print(shoutdown_time)
-    print(shoutdown_time)
-
-
+# TODO:
+# Rework config reading
+# Attach an 7-element display to display remaining time
 asyncio.run(main())
--- messageHandler.py
+++ messageHandler.py
@@ -1,19 +1,29 @@
 import telethon as tl
 import asyncio
+import re
+import time
 
-async def get_shoutdown_time(api_id, api_hash):
+async def get_shutdown_periods(api_id: int, api_hash: str, source: str, shutdown_query: str, message_pattern: str) -> str:
     client = tl.TelegramClient('anon', api_id, api_hash)
     queue = asyncio.Queue()
 
-    @client.on(tl.events.NewMessage(outgoing=True))
-    async def handler(event):
-        await queue.put(event.message)  # every new message stores here
-
+    # Telegram client activation
     await client.start()
+    asyncio.create_task(client.run_until_disconnected())
 
-    client.run_until_disconnected()
 
-    # message reader
-    while True:
+    @client.on(tl.events.NewMessage(from_users=source, pattern =r"(?s).*{0}".format(message_pattern) ))
+    async def handler(event):
+        await queue.put(event.message)  # Every new message stores here
+
+    # Message reader
+    while client.is_connected():
         msg = await queue.get()
-        yield msg  # returns message when asked
+        print(msg)
+        a = r"(?m)^" + re.escape(shutdown_query) + r".*$"
+        shutdown_period = re.search(r"(?m)^" + re.escape(shutdown_query) + r".*$", msg.message)
+        shutdown_period = [i.split(' - ') for i in shutdown_period.group(0)[4:].split(', ')]
+        yield shutdown_period  # Returns message when asked
+    
+
+    
